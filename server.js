46b7dfa1ea581eaea56fefd074fc67f2d46920b3
Deepak <rajd3909@gmail.com>
2024-03-27 19:30:09 +0530

    login

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ import bodyParser from "body-parser";
 import { MongoClient } from 'mongodb';
 import cors from "cors";
 import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 
 const app = express();
 app.use(cors())
+
+const auth=(req,res,next)=>{
+    try{
+    const token = req.header("backend-token");//keyname,assign value as token
+    jwt.verify(token,"abcd");
+    next();
+    }
+    catch(error) {
+    res.status(401).send({message:error.message});
+    }
+    }
+
 
 const url = "mongodb+srv://deera3468:wQYHszmzEZiYK4tD@back153.i8nqqzc.mongodb.net/?retryWrites=true&w=majority&appName=back153";
 const client = new MongoClient(url);
@@ -48,7 +62,7 @@ app.post('/postcomment', async (req, res) => {
 
     res.status(200).json(updatedQuestion.value); 
 });
-app.get('/getmany', async (req,res)=>{
+app.get('/getmany',auth, async (req,res)=>{
     const getdata= await client.db("stack").collection("questions").find({}).toArray();
     res.status(200).send(getdata);
 });
@@ -63,6 +77,41 @@ app.get('/detials/:id', async (req, res) => {
     const getdata = await client.db("stack").collection("questions").findOne({_id:new ObjectId(id)});
     res.status(200).send(getdata);
 });
+app.post("/register",async (req, res)=>{
+    const {username,email,password} = req.body;
+    const userfind=await client.db("CURD").collection("registerdata").findOne({email:email});
+    if (!userfind){
+    const salt=await bcrypt.genSalt(10);
+    const encrypt=await bcrypt.hash(password,salt);
+    const registerData = await client.db("CURD").collection("registerdata").insertOne({name:username,email:email,password:encrypt});
+    res.status(201).send(registerData);
+    
+    }
+    else{
+        res.status(400).send("user already registerd");
+    }
+    
+    
+})
+app.post('/login', async(req, res) => {
+    const {email,password} = req.body;
+    const userfind=await client.db("CURD").collection("registerdata").findOne({email:email});
+    if (userfind){
+        const mongopass=userfind.password;
+        const check= await bcrypt.compare(password, mongopass);
+        if(check){
+            const token=jwt.sign({id:userfind._id},"abcd");// jwt token abcd
+            res.status(200).send({token:token})
+        }
+        else{
+            res.status(400).send({message:"not Verified user"});
+        }
+    }
+    else{
+        res.status(400).send({message:"User not Found"});
+    }
+})
+;
 
 app.listen(4000, () => {
     console.log("Server is running on http://localhost:4000");
